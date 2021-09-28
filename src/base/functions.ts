3f8cf2aa60cf745411dd093e5c9dea75bbf14b5d
Ghislain Jezequel <jezequel@dessia.tech>
2021-09-28 19:03:30 +0200

    only move class

--- src/base/functions.ts
+++ src/base/functions.ts
@@ -1,6 +1,6 @@
-import { string_to_hex_dict } from "./constants";
+// import { string_to_hex_dict } from "./constants";
 
-export function string_to_hex(str:string): string {
-    if (!Object.keys(string_to_hex_dict).includes(str)) {throw new Error('string_to_hex -> Invalid color : ' + str + ' not in list');}
-    return string_to_hex_dict[str];
-  }
+// export function string_to_hex(str:string): string {
+//     if (!Object.keys(string_to_hex_dict).includes(str)) {throw new Error('string_to_hex -> Invalid color : ' + str + ' not in list');}
+//     return string_to_hex_dict[str];
+//   }
--- src/base/shape.ts
+++ src/base/shape.ts
@@ -1,4 +1,13 @@
-import { string_to_hex } from "./functions";
+// import { string_to_hex } from "./functions";
+
+const string_to_hex_dict = {red:'#f70000', lightred:'#ed8080', blue:'#0013fe', lightblue:'#c3e6fc', lightskyblue:'#87cefa', green:'#00c112', lightgreen:'#89e892', yellow:'#f4ff00', lightyellow:'#f9ff7b', orange:'#ff8700',
+lightorange:'#ff8700', cyan:'#13f0f0', lightcyan:'#90f7f7', rose:'#ff69b4', lightrose:'#ffc0cb', violet:'#ee82ee', lightviolet:'#eaa5f6', white:'#ffffff', black:'#000000', brown:'#cd8f40',
+lightbrown:'#deb887', grey:'#a9a9a9', lightgrey:'#d3d3d3'};
+
+function string_to_hex(str:string): string {
+  if (!Object.keys(string_to_hex_dict).includes(str)) {throw new Error('string_to_hex -> Invalid color : ' + str + ' not in list');}
+  return string_to_hex_dict[str];
+}
 
 /** 
  * A generic toolbox class that contains numerous geometrical static functions.

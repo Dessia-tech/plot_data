31dc7c7b1e4f7c3a0ad3010be7623d1e629dd80f
Ghislain Jezequel <jezequel@dessia.tech>
2021-10-13 18:31:22 +0200

    build(webpack): add config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/core.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "plot-data.js",
+    library: {
+      name: "PlotData",
+      type: "umd",
+      umdNamedDefine: true
+    }
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};

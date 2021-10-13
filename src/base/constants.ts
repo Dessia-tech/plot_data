219f522db43293fe3626cd2c5fddd3185b1c7ca2
Ghislain Jezequel <jezequel@dessia.tech>
2021-10-13 18:32:23 +0200

    refactor(webpack): small split. move small function to separate modules

--- src/base/constants.ts
+++ src/base/constants.ts
@@ -1,3 +1,5 @@
-// export const string_to_hex_dict = {red:'#f70000', lightred:'#ed8080', blue:'#0013fe', lightblue:'#c3e6fc', lightskyblue:'#87cefa', green:'#00c112', lightgreen:'#89e892', yellow:'#f4ff00', lightyellow:'#f9ff7b', orange:'#ff8700',
-//   lightorange:'#ff8700', cyan:'#13f0f0', lightcyan:'#90f7f7', rose:'#ff69b4', lightrose:'#ffc0cb', violet:'#ee82ee', lightviolet:'#eaa5f6', white:'#ffffff', black:'#000000', brown:'#cd8f40',
-//   lightbrown:'#deb887', grey:'#a9a9a9', lightgrey:'#d3d3d3'};
+export const string_to_hex_dict = {
+    red: '#f70000', lightred: '#ed8080', blue: '#0013fe', lightblue: '#c3e6fc', lightskyblue: '#87cefa', green: '#00c112', lightgreen: '#89e892', yellow: '#f4ff00', lightyellow: '#f9ff7b', orange: '#ff8700',
+    lightorange: '#ff8700', cyan: '#13f0f0', lightcyan: '#90f7f7', rose: '#ff69b4', lightrose: '#ffc0cb', violet: '#ee82ee', lightviolet: '#eaa5f6', white: '#ffffff', black: '#000000', brown: '#cd8f40',
+    lightbrown: '#deb887', grey: '#a9a9a9', lightgrey: '#d3d3d3'
+};
--- src/core.ts
+++ src/core.ts
@@ -1,15 +1,6 @@
 import { Shape } from "./base/shape";
-// import { string_to_hex } from "./base/functions";
-// import { string_to_hex_dict } from "./base/constants";
-
-const string_to_hex_dict = {red:'#f70000', lightred:'#ed8080', blue:'#0013fe', lightblue:'#c3e6fc', lightskyblue:'#87cefa', green:'#00c112', lightgreen:'#89e892', yellow:'#f4ff00', lightyellow:'#f9ff7b', orange:'#ff8700',
-lightorange:'#ff8700', cyan:'#13f0f0', lightcyan:'#90f7f7', rose:'#ff69b4', lightrose:'#ffc0cb', violet:'#ee82ee', lightviolet:'#eaa5f6', white:'#ffffff', black:'#000000', brown:'#cd8f40',
-lightbrown:'#deb887', grey:'#a9a9a9', lightgrey:'#d3d3d3'};
-
-function string_to_hex(str:string): string {
-  if (!Object.keys(string_to_hex_dict).includes(str)) {throw new Error('string_to_hex -> Invalid color : ' + str + ' not in list');}
-  return string_to_hex_dict[str];
-}
+import { string_to_hex } from "./base/functions";
+import { string_to_hex_dict } from "./base/constants";
 
 var multiplot_saves:MultiplePlots[]=[];
 var current_save:number=0;

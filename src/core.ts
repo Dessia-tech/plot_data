526fd55bb946318cfb27b6bc9da7498c3baac5d2
jezequel <jezequel@dessia.tech>
2021-12-30 11:25:54 +0100

    build: change config filenames

--- src/core.ts
+++ src/core.ts
@@ -3,13 +3,9 @@ export * from './subplots';
 
 // Temporary imports below
 // This document is used as a barrel file for instance as no solution has been found yet to use multiple js files.
-// export * from "./color_conversion";
+export * from "./color_conversion";
 export * from "./plot-data";
 export * from "./primitives";
 export * from "./style";
 export * from "./toolbox";
 export * from "./utils";
-
-import * as ColorConversion from "./color_conversion";
-
-export { ColorConversion }

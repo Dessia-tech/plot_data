7726fa5a568f8694eee1d27313e98a1b253c04d3
Balthazar Wilson <balthazar.wilson@etu.utc.fr>
2022-10-12 16:49:15 +0200

    add: width so we can see there is no color

--- script/arc_test.py
+++ script/arc_test.py
@@ -1,30 +1,32 @@
 import plot_data
 import math
 
-from plot_data.colors import *
+import plot_data.colors as colors
 
-arc1 = {'name': '', 'package_version': '0.10.2', 'cx': 0.0, 'cy': 0.0, 'r': 0.022500000000000003,
+
+arc1 = {'name': '', 'cx': 0.0, 'cy': 0.0, 'r': 0.022500000000000003,
         'start_angle': -1.4594553124539327, 'end_angle': -1.6821373411358604,
         'data': [{'x': -0.0025, 'y': -0.0223606797749979},  {'x': 0.0025, 'y': -0.0223606797749979}],
-        'anticlockwise': False, 'type_': 'arc'}
+        'anticlockwise': False}
 
-arc2 = {'name': '', 'package_version': '0.10.2', 'cx': 0.0, 'cy': 0.0, 'r': 0.022500000000000003,
+arc2 = {'name': '', 'cx': 0.0, 'cy': 0.0, 'r': 0.022500000000000003,
         'start_angle': -1.4594553124539327, 'end_angle': -1.6821373411358604,
         'data': [{'x': -0.0025, 'y': -0.0223606797749979},  {'x': 0.0025, 'y': -0.0223606797749979}],
-        'anticlockwise': True, 'type_': 'arc'}
+        'anticlockwise': True}
 
-arc3 = {'name': '', 'package_version': '0.10.2', 'cx': 0.0, 'cy': 0.0, 'r': 0.022500000000000003,
-        'start_angle': -1.4594553124539327, 'end_angle': -1.6821373411358604 - 2*math.pi,
+arc3 = {'name': '', 'cx': 0.0, 'cy': 0.0, 'r': 0.022500000000000003,
+        'start_angle': -1.4594553124539327 - 2*math.pi, 'end_angle': -1.6821373411358604 - 2*math.pi,
         'data': [{'x': -0.0025, 'y': -0.0223606797749979},  {'x': 0.0025, 'y': -0.0223606797749979}],
-        'anticlockwise': False, 'type_': 'arc'}
+        'anticlockwise': False}
 
 
-arc_obj1 = plot_data.Arc2D.dict_to_object(arc1)
-arc_obj1.edge_style = plot_data.EdgeStyle(color_stroke=BLUE, dashline=[10, 5])  # How does this work??
-arc_obj2 = plot_data.Arc2D.dict_to_object(arc2)
-arc_obj2.edge_style = plot_data.EdgeStyle(color_stroke=RED, dashline=[10, 5])
-arc_obj3 = plot_data.Arc2D.dict_to_object(arc3)
-arc_obj3.edge_style = plot_data.EdgeStyle(color_stroke=GREEN, dashline=[10, 5])
-primitives = plot_data.PrimitiveGroup(primitives=[arc_obj2])
+arc_obj1 = plot_data.Arc2D(edge_style=plot_data.EdgeStyle(color_stroke=colors.BLUE, line_width=5), **arc1)
+arc_obj2 = plot_data.Arc2D(edge_style=plot_data.EdgeStyle(color_stroke=colors.YELLOW, line_width=5), **arc2)
+arc_obj3 = plot_data.Arc2D(edge_style=plot_data.EdgeStyle(color_stroke=colors.GREEN, line_width=5), **arc3)
+primitive1 = plot_data.PrimitiveGroup(primitives=[arc_obj1])
+primitive2 = plot_data.PrimitiveGroup(primitives=[arc_obj2])
+primitive3 = plot_data.PrimitiveGroup(primitives=[arc_obj3])
+
+primitives = plot_data.PrimitiveGroupsContainer(primitive_groups=[primitive1, primitive2, primitive3])
 
 plot_data.plot_canvas(plot_data_object=primitives)

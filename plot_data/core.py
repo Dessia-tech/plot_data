e898a280a2fbdba48a419ece5a7d6dbd0f7eaedb
jezequel <jezequel@dessia.tech>
2022-12-29 10:25:44 +0100

    feat(refpath): add serialization

--- plot_data/core.py
+++ plot_data/core.py
@@ -87,6 +87,18 @@ class Sample(PlotDataObject):
 
         PlotDataObject.__init__(self, type_="sample", name=name)
 
+    def to_dict(self, use_pointers: bool = False, memo=None, path: str = '#') -> JsonSerializable:
+        dict_ = {"path": self.path, "name": self.name}
+        dict_.update(self.values)
+        return dict_
+
+    @classmethod
+    def dict_to_object(cls, dict_: JsonSerializable, force_generic: bool = False, global_dict=None,
+                       pointers_memo: Dict[str, Any] = None, path: str = '#') -> 'DessiaObject':
+        path = dict_.pop("path")
+        name = dict_.pop("name")
+        return cls(values=dict_, path=path, name=name)
+
     def plot_data(self):
         raise NotImplementedError("Method plot_data is not defined for class Sample.")
 

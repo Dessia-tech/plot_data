5cd641f958f59749e2335a621fbd15eddd74e897
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-01-30 14:12:32 +0100

    test(cypress): finally manage to compare generated png from directly built png from html screenshot

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,14 +1,8 @@
-import * as fs from "fs";
-import prettier from "prettier";
-import { PlotScatter } from '../../src/subplots';
 import { htmlString } from '../templates/test';
 import { plotScatterData } from '../data_src/plotScatterData';
 import { HtmlWriter } from '../support/HtmlWriter'
 
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
   it("take screenshot", () => {
     let version = plotScatterData['package_version'];
     let core_path = 'https://cdn.dessia.tech/js/plot-data/v' + version + '/plot-data.js';
--- cypress/support/HtmlWriter.ts
+++ cypress/support/HtmlWriter.ts
@@ -1,6 +1,8 @@
 export function HtmlWriter(html: string, core_path: string, data: any, canvas_id: string) {
     html = html.replace("$data", JSON.stringify(data));
     html = html.replace("$core_path", core_path);
-    html = html.replace(/$canvas_id/g, canvas_id);
+    html = html.replace("$canvas_id", canvas_id);
+    html = html.replace("$canvas_id", canvas_id);
+    html = html.replace("$canvas_id", canvas_id);
     return html
 }

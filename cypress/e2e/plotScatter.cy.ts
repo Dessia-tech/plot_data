f418abe1ae98c3bd9764259a7edd58764a43c7cf
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-02-03 16:01:10 +0100

    finally working tests with re-compiled library at each test, with visual regression

--- cypress/e2e/plotScatter.cy.ts
+++ cypress/e2e/plotScatter.cy.ts
@@ -1,15 +1,11 @@
 import { htmlString } from '../templates/plotScatter.template';
 import { HtmlWriter } from '../support/HtmlWriter';
 import { plotScatterData } from '../data_src/plotScatter.data';
-import { PlotData } from '../../libdev/plot-data';
 
 describe('template spec', () => {
   const fileName = "plotScatter"
   it("take screenshot", () => {  
-    // const loc = 'https://localhost:' + Cypress.config('port') + '/plot_data'; //Cypress.config('fileServerFolder');
-    const loc = Cypress.config('projectRoot');
-    const corePath = loc + '/libdev/plot-data.js';
-    // const corePath = "/__cypress/tests?p=libdev/plot-data.js"
+    const corePath = 'http://localhost:3030/libdev/plot-data.js';
     const htmlFile = HtmlWriter(htmlString, corePath, plotScatterData, 'canvas');
     cy.writeFile("cypress/html_files/" + fileName + ".html", htmlFile, 'utf-8');
     cy.visit("cypress/html_files/" + fileName + ".html");

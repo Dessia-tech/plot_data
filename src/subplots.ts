d7eeef2cccaa2aa5f6e2e0a0195b82240f1cf80a
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-07-05 20:29:47 +0200

    feat(selection): reset selectionbox

--- src/subplots.ts
+++ src/subplots.ts
@@ -1581,6 +1581,7 @@ export class BasePlot extends PlotData {
 
   public reset(): void {
     this.axes.forEach(axis => axis.reset());
+    this.selectionBox = new SelectionBox();
     this.initSelectors();
   }
 

812b7241e943fcd4605ded9cb27940cf4d09ddd4
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-07-20 17:22:37 +0200

    regenerate tests

--- script/plot_scatter.py
+++ script/plot_scatter.py
@@ -10,7 +10,7 @@ random.seed(2)
 elements = []
 SHAPES = ['round', 'square', 'triangle', 'ellipse']
 COLORS = [RED, BLUE, GREEN, YELLOW, ORANGE, VIOLET]
-for i in range(50):
+for i in range(1000):
     random_shape = SHAPES[random.randint(0, len(SHAPES) - 1)]
     random_color = COLORS[random.randint(0, len(SHAPES) - 1)]
     elements.append({'mass': random.uniform(0, 50),
@@ -18,8 +18,12 @@ for i in range(50):
                      'shape': random_shape,
                      'color': random_color})
 
+points_sets = [plot_data.PointFamily(plot_data.colors.RED, [0,1,2,3,4,5,6,7,8,9]),
+               plot_data.PointFamily(plot_data.colors.YELLOW, [10,11,12,13,14,15,16,17,18,19]),
+               plot_data.PointFamily(plot_data.colors.GREEN, [20,21,22,23,24,25,26,27,28,29]),
+               plot_data.PointFamily(plot_data.colors.PINK, [30,31,32,33,34,35,36,37,38,39])]
 
-plot_data_object = plot_data.Scatter(elements=elements,
+plot_data_object = plot_data.Scatter(elements=elements, points_sets=points_sets,
                                      x_variable='mass', y_variable='length')
 
 # The previous scripts shows the simplest way of creating a scatterplot.
@@ -65,11 +69,6 @@ tooltip = plot_data.Tooltip(attributes=['mass', 'length', 'shape'])
 # Heatmap settings
 heatmap = plot_data.Heatmap([4, 2], colors=[YELLOW, ORANGE, RED])
 
-points_sets = [plot_data.PointFamily(plot_data.colors.RED, [0,1,2,3,4,5,6,7,8,9]),
-               plot_data.PointFamily(plot_data.colors.YELLOW, [10,11,12,13,14,15,16,17,18,19]),
-               plot_data.PointFamily(plot_data.colors.GREEN, [20,21,22,23,24,25,26,27,28,29]),
-               plot_data.PointFamily(plot_data.colors.PINK, [30,31,32,33,34,35,36,37,38,39])]
-
 # Now, here is the new scatterplot
 customized_scatterplot = plot_data.Scatter(x_variable='mass', y_variable='shape',
                                            point_style=point_style,
@@ -80,4 +79,4 @@ customized_scatterplot = plot_data.Scatter(x_variable='mass', y_variable='shape'
                                            heatmap=heatmap)
 
 # if debug_mode is True, set it to False
-plot_data.plot_canvas(plot_data_object=customized_scatterplot, debug_mode=True)
+plot_data.plot_canvas(plot_data_object=plot_data_object, debug_mode=True)

da00920bd48d8612554e1d9b2923a6f4c3780a55
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-10-06 18:01:34 +0200

    feat(primitive): regenerate tests. Needs some improvements for simple shapes

--- cypress/e2e/simpleshapes.cy.ts
+++ cypress/e2e/simpleshapes.cy.ts
@@ -20,37 +20,29 @@ describe('SIMPLE SHAPES CANVAS', function () {
   it("should color hovered circle", function () {
     cy.window().then((win) => {
       let plot_data = win.eval('plot_data');
-      cy.get(plot_data)
-      .invoke('attr', 'select_on_mouse', plot_data.plot_datas[0].primitives[4])
+      cy.get('canvas').click(544, 376)
       .then( () => {
-        plot_data.draw()
-        cy.compareSnapshot(describeTitle + this.test.title, 0.05);
+        expect(plot_data.relativeObjects.shapes[21].isClicked).to.be.true;
       })
     })
   })
 
-  it("should draw tooltip on line", function () {
-    cy.window().then((win) => {
-      let plot_data = win.eval('plot_data');
-      cy.get(plot_data)
-      .invoke('attr', 'select_on_mouse', plot_data.plot_datas[0].primitives[6])
-      .then( () => {
-        plot_data.draw()
-        plot_data.select_on_mouse.tooltip.draw_primitive_tooltip(
-          plot_data.context, plot_data.scale, plot_data.originX, plot_data.originY,
-          plot_data.X, plot_data.Y, 595, 300, plot_data.width, plot_data.height);
-        cy.compareSnapshot(describeTitle + this.test.title, 0.05);
-      })
-    })
-  })
+  // it("should draw tooltip on line", function () {
+  //   cy.window().then((win) => {
+  //     let plot_data = win.eval('plot_data');
+  //     plot_data.relativeObjects.shapes[23].isClicked = true;
+  //     plot_data.draw();
+  //     cy.compareSnapshot(describeTitle + this.test.title, 0.05);
+  //   })
+  // })
 
-  it("should be hovered near line", function () {
-    cy.window().then((win) => {
-      let plot_data = win.eval('plot_data');
-      cy.get('canvas').click(572, 275)
-      .then( () => {
-        expect(plot_data.select_on_click[0]).to.equal(plot_data.plot_datas[0].primitives[6]);
-      })
-    })
-  })
+  // it("should be hovered near line", function () {
+  //   cy.window().then((win) => {
+  //     let plot_data = win.eval('plot_data');
+  //     cy.get('canvas').click(572, 275)
+  //     .then( () => {
+  //       expect(plot_data.select_on_click[0]).to.equal(plot_data.plot_datas[0].primitives[6]);
+  //     })
+  //   })
+  // })
 })

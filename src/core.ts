98e664146e7a5dc8cce425a99da40edf082d0d17
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-11-07 12:35:36 +0100

    feat(multiplot): remove unused code

--- src/core.ts
+++ src/core.ts
@@ -1,9 +1,5 @@
 export * from "./multiplots";
 export * from './subplots';
-
-// Temporary imports below
-// This document is used as a barrel file for instance as no solution has been found yet to use multiple js files.
 export * from "./color_conversion";
 export * from "./style";
-export * from "./toolbox";
 export * from "./utils";

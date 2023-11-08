32db54b0951f6c1c00a5770c738ff58a8e0bd954
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-11-08 15:14:17 +0100

    add new files

---
+++ src/shapeFunctions.ts
@@ -0,0 +1,38 @@
+import { Vertex, Shape } from "./baseShape"
+import { Arc, Circle, Rect, RoundRect, LineSegment, Line, Contour, LineSequence, Point } from "./primitives"
+import { Text, Label, Tooltip } from "./shapes"
+
+export function deserialize(data: { [key: string]: any }, scale: Vertex): Shape {
+  let shape: Shape;
+  if (data.type_ == "circle") shape = Circle.deserialize(data, scale)
+  else if (data.type_ == "contour") shape = Contour.deserialize(data, scale);
+  else if (data.type_ == "line2d") shape = Line.deserialize(data, scale);
+  else if (data.type_ == "linesegment2d") shape = LineSegment.deserialize(data, scale);
+  else if (data.type_ == "wire") shape = LineSequence.deserialize(data, scale);
+  else if (data.type_ == "point") shape = Point.deserialize(data, scale);
+  else if (data.type_ == "arc") shape = Arc.deserialize(data, scale);
+  else if (data.type_ == "text") return Text.deserialize(data, scale);
+  else if (data.type_ == "label") {
+    const labelShape = data.shape ? deserialize(data.shape, scale) : new Rect();
+    const fakeLegend = Label.deserialize(data, scale);
+    shape = new Label(labelShape, fakeLegend.text);
+  }
+  else if (data.type_ == "rectangle") shape = Rect.deserialize(data, scale);
+  else if (data.type_ == "roundrectangle") shape = RoundRect.deserialize(data, scale);
+  else throw new Error(`${data.type_} deserialization is not implemented.`);
+  console.log(shape, data)
+  shape.deserializeStyle(data)
+  return shape
+}
+
+export function initializeTooltip(shape: Shape, context: CanvasRenderingContext2D): Tooltip {
+  shape.initTooltipOrigin();
+  const tooltip = new Tooltip(shape.tooltipOrigin, shape.tooltipMap, context);
+  tooltip.setFlip(shape);
+  return tooltip
+}
+
+export function styleToLegend(shape: Shape, legendOrigin: Vertex, legendSize: Vertex): Shape {
+  if (!shape.isFilled) return new LineSegment(legendOrigin.copy(), legendOrigin.add(legendSize))
+  return new Rect(legendOrigin.copy(), legendSize);
+}

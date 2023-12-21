98f2171bf8001e60ccb3093da057bb10cf009784
Tanguy-DES <tanguy.loreau@dessia.tech>
2023-12-21 10:49:36 +0100

    feat(dates): remove unused import

--- plot_data/core.py
+++ plot_data/core.py
@@ -12,7 +12,7 @@ import datetime
 import tempfile
 import warnings
 import webbrowser
-from typing import Dict, List, Tuple, Union, Any
+from typing import Dict, List, Tuple, Union
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon, Circle, Arc

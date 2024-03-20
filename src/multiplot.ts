ad1bf1a0f7ad2ef49bab629499a395f306739d98
Tanguy-DES <tanguy.loreau@dessia.tech>
2024-03-20 15:27:30 +0100

    fix(add_plot_no_name): remove useless lines

--- src/multiplot.ts
+++ src/multiplot.ts
@@ -71,9 +71,7 @@ export class Multiplot {
     const elements = [];
     for (let i=0; i < this.nSamples; i++) {
       const newSample = {};
-      this.featureNames.forEach(feature => {
-        if (feature != "name") newSample[feature] = this.features.get(feature)[i];
-      });
+      this.featureNames.forEach(feature =>  newSample[feature] = this.features.get(feature)[i]);
       newSample["values"] = {...newSample};
       elements.push(newSample);
     }
